ca4906b0b13b54e34ef3e9788f44966a06b9eeac
jsapp18 <jsapp18@ivytech.edu>
2024-06-02 20:24:56 -0400

    added comments

--- distance_converter.java
+++ distance_converter.java
@@ -1,14 +1,17 @@
 
 class distance_converter {
+    //converts a foot to a meter
     public static double footToMeter(double foot){
         double meter = 0.305 * foot;
         return meter;
     };
+    //converts a meter to a foot
     public static double meterToFoot(double meter){
         double foot = 3.279 * meter;
         return foot;
     };
 
+//Main houses the loops that generate the tables, and run the required functions
     public static void main(String[] args) {
         double footstarter = 1.0;
         double meterstarter = 20.0;
